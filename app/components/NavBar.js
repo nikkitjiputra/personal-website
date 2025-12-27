cfd0afe097d9a8cd93a863f0d68d5754badeaac6
nikkitjiputra <115187779+nikkitjiputra@users.noreply.github.com>
2025-12-27 16:58:31 -0500

    trying mobile features

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,15 +1,43 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 import Link from 'next/link';
+import Image from 'next/image';
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  //console.log(isOpen);
   return (
-    <div className="bg-orange-100 flex flex-row justify-center text-2xl py-5 pr-5">
+    <div>
+      <nav className="md:hidden bg-orange-100 p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className=" self-start">Nikki Tjiputra</div>
+          <div className="flex flex-col">
+            <button id="menu-button" onClick={() => setIsOpen(!isOpen)} className="md:hidden self-end text-black">
+              <Image src="/hamburger.png" width="15" height="15" alt="Menu" />
+            </button>
+            {isOpen &&
+            <div id="menu" className="flex flex-col md:items-center mt-10 text-xl space-x-4">
+              <Link href="/" className="text-black m-2">Home</Link>
+              <Link href="/about" className="text-black m-2">About</Link>
+              <Link href="/experience" className="text-black m-2">Experience</Link>
+              <Link href="/projects" className="text-black m-2">Projects</Link>
+              <Link href="/contact" className="text-black m-2">Contact</Link>
+            </div>}
+          </div>
+          
+        </div>
+      </nav>
+
+      <div className="hidden md:block bg-orange-100 flex flex-row justify-center text-2xl py-5 pr-5">
         <Link className="mx-10 hover:text-shadow-lg" href="/">Home</Link>
         <Link className="mx-10 hover:text-shadow-lg" href="/about">About</Link>
         <Link className="mx-10 hover:text-shadow-lg" href="/experience">Experience</Link>
         <Link className="mx-10 hover:text-shadow-lg" href="/projects">Projects</Link>
         <Link className="mx-10 hover:text-shadow-lg" href="/contact">Contact</Link>
+      </div>
+
     </div>
+    
   )
 }
 
--- app/page.js
+++ app/page.js
@@ -1,24 +1,25 @@
 import NavBar from "./components/NavBar";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col h-screen">
-      <NavBar />
-      <div className="flex grow h-screen">
+      <NavBar className="" />
+      <div className="flex flex-col grow h-screen md:flex md:flex-row md:grow md:h-screen">
 
       
-        <div className="mx-50 mt-30 w-200 p-10">
-          <h1 className="text-2xl mb-10">Hi, my name is</h1>
-          <h1 className="text-6xl text-amber-900 mb-5">Nikki Tjiputra</h1>
-          <h1 className="text-3xl mb-5">University of Maryland | Computer Science</h1>
+        <div className="mr-10 p-5 mt-5 w-full md:mx-50 md:mt-40 md:w-1/2 md:p-10">
+          <h1 className="mb-5 md:text-2xl md:mb-10">Hi, my name is</h1>
+          <h1 className="text-5xl mb-3 md:text-6xl text-amber-900 md:mb-5">Nikki Tjiputra</h1>
+          <h1 className="text-xl mb-3 md:text-3xl md:mb-5">University of Maryland | Computer Science</h1>
           <h1>I'm an aspiring software engineer with strong foundations in full-stack 
             development, machine learning, and data processing. I'm passionate about 
             building impactful software that balances innovation with reliability and 
             sustainability.</h1>
         </div>
-        <div className="flex justify-end items-end grow h-full">
-          <Image src="/flowers.png" width="500" height="500" alt="Flowers"></Image>
+        <div className="flex justify-end items-end grow md:h-full">
+          <Image className="w-full h-auto rounded-xl md:w-auto md:h-auto" src="/flowers.png" width="500" height="500" alt="Flowers"></Image>
         </div>
       </div>
       
