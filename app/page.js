3e171eec747aedcba02e3f7014b10952e1cf2ac5
nikkitjiputra <115187779+nikkitjiputra@users.noreply.github.com>
2026-01-20 13:50:18 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
 
         <div className="mr-10 p-5 mt-20 w-full md:mx-50 md:mt-40 md:w-1/2 md:pb-0 md:px-10 md:pt-10">
           <h1 className="mb-5 md:text-2xl font-serif md:mb-10">Hi, my name is</h1>
-          <h1 className="text-5xl mb-5 font-serif md:text-6xl text-amber-900">Nikki Tjiputra</h1>
+          <h1 className="text-5xl mb-5 font-serif md:text-6xl text-amber-900">Lauren (Nikki) Tjiputra</h1>
           <h1 className="text-xl mb-5 font-serif md:text-3xl">University of Maryland | Computer Science</h1>
           <h1 className="font-serif">I'm an aspiring software engineer with strong foundations in full-stack 
             development, machine learning, and data processing. I'm passionate about 
